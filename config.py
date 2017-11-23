9a940ad9677db386a21b1dab24479126f2a643da
shresthgupta29 <33654121+shresthgupta29@users.noreply.github.com>
2017-11-23 12:28:26 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+cwd = os.getcwd()  #/current working directory
+java_home='/scratch/jdk1.8.0_144/'
+oracle_home = '/u01/app/oracle/product/12.1.0/dbhome_1'
+oracle_base = '/u01/app/oracle' 
+host = '10.19.167.253' #host name of current machine
+client_path = '/scratch/Client/database' # path of 'database' folder containing 'runInstaller.sh'
+admin_pass='Manager1' # password for sys/system/admin accounts
+cdb = 'db1712' #container db name
+
