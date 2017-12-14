5c797641f092d981e4190050851b321c5b360c70
shresthgupta29 <33654121+shresthgupta29@users.noreply.github.com>
2017-12-14 17:06:49 +0530

    Update machines.py

--- machines.py
+++ machines.py
@@ -1,6 +1,6 @@
 HA = 1			#put HA = 1 for HA environment and 0 for Non_HA
 
-IDM_NodeA= ["10.120.0.6","10.75.35.146"]
+IDM_NodeA= ["10.120.0.6","10.75.35.146"]   #Internal IP,External IP
 App01_NodeA = ["10.120.0.7","10.75.35.137"]
 App02_NodeA = ["10.120.0.5","10.75.35.207"]
 App03_NodeA = ["10.120.0.4","10.120.0.90"]
@@ -10,9 +10,9 @@ App01_NodeB = ["10.120.0.2","10.75.35.137"]
 App02_NodeB = ["10.120.0.8","10.75.35.137"]
 App03_NodeB = ["10.120.0.4","10.75.35.137"]
 
-nfs='10.120.0.2'
+nfs='10.120.0.2' #internal ip of nfs
 
-db=["10.120.0.3","10.75.35.221"]
+db=["10.120.0.3","10.75.35.221"] #Internal IP,External IP
 
 zip_path = "" #path of the chefToolkit with the file itself Example : "/u02/app_files/PrimeeraXXXXXXXXXX.zip"  
 chef_path="/home/cloud-user/Primavera_Orchestration_toolkit_17.11.0.0"  #till Cheforchestration --> this folder should contain chef and testing folders
