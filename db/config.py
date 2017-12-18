f8c4c8debd9cef02b70d7b999c5b7e2bf74e2673
shresthgupta29 <33654121+shresthgupta29@users.noreply.github.com>
2017-12-18 12:24:50 +0530

    Update config.py

--- db/config.py
+++ db/config.py
@@ -4,7 +4,7 @@ java_home='/scratch/jdk1.8.0_144/'
 oracle_home = '/u01/app/oracle/product/12.1.0/dbhome_1'
 oracle_base = '/u01/app/oracle' 
 host = '10.19.164.164' #host name of current machine
-client_path = '/scratch/Client/database' # path of 'database' folder containing 'runInstaller.sh'
+client_path = cwd+'/database' # path of 'database' folder containing 'runInstaller.sh'
 admin_pass='Manager1' # password for sys/system/admin accounts
 cdb = 'cdb1712' #container db name
 
